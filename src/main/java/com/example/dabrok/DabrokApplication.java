639bb700a9b401c92159a2442de85f4c9834ba92
RubCaMo <rubencamo7@gmail.com>
2023-03-03 15:36:26 +0100

    Changes to dissable spring security config + validated connection to API + Database. Example endpoint: http://localhost:8080/dabrok/person/all

--- src/main/java/com/example/dabrok/DabrokApplication.java
+++ src/main/java/com/example/dabrok/DabrokApplication.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class }) //Exlude para evitar el uso del token de seguridad.
 @EnableConfigurationProperties
 @ComponentScan(basePackages = {"com.example.dabrok.services", "com.example.dabrok.controller"})
 public class DabrokApplication implements WebMvcConfigurer {
